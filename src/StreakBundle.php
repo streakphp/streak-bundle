66e3a68c5668ec76ef52f56f0bacdc59905909c5
Alan Gabriel Bem <alan.bem@gmail.com>
2017-11-21 11:07:57 +0100

    empty bundle

---
+++ src/StreakBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the streak-bundle package.
+ *
+ * (C) Alan Gabriel Bem <alan.bem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Streak\StreakBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Alan Gabriel Bem <alan.bem@gmail.com>
+ */
+class StreakBundle extends Bundle
+{
+
+}
