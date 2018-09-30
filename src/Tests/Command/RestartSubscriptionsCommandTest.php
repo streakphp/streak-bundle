e14285a7e51644fb97edfdde121afd8c64bfb296
Alan Gabriel Bem <alan.bem@gmail.com>
2018-09-30 17:04:10 +0200

    full list of changes below: - upgraded streak - upgraded console command that runs subscriptions - added console command for restarting subscriptions - configured system clock

---
+++ src/Tests/Command/RestartSubscriptionsCommandTest.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * This file is part of the streak-bundle package.
+ *
+ * (C) Alan Gabriel Bem <alan.bem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Streak\StreakBundle\Tests\Command;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Streak\Domain\Event;
+use Streak\Domain\Event\Subscription\Exception;
+use Streak\Domain\Event\Subscription\Repository;
+use Streak\Infrastructure\UnitOfWork;
+use Streak\StreakBundle\Command\RestartSubscriptionsCommand;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Alan Gabriel Bem <alan.bem@gmail.com>
+ *
+ * @covers \Streak\StreakBundle\Command\RestartSubscriptionsCommand
+ */
+class RestartSubscriptionsCommandTest extends TestCase
+{
+    /**
+     * @var Repository|MockObject
+     */
+    private $repository;
+
+    /**
+     * @var UnitOfWork
+     */
+    private $uow;
+
+    /**
+     * @var Event\Subscription|MockObject
+     */
+    private $subscription1;
+
+    /**
+     * @var Event\Subscription|MockObject
+     */
+    private $subscription2;
+
+    /**
+     * @var Event\Subscription|MockObject
+     */
+    private $subscription3;
+
+    /**
+     * @var Event\Subscription|MockObject
+     */
+    private $subscription4;
+
+    /**
+     * @var OutputInterface|MockObject
+     */
+    private $output;
+
+    public function setUp()
+    {
+        $this->repository = $this->getMockBuilder(Repository::class)->getMockForAbstractClass();
+        $this->uow = $this->getMockBuilder(UnitOfWork::class)->getMockForAbstractClass();
+
+        $this->subscription1 = $this->getMockBuilder(Event\Subscription::class)->getMockForAbstractClass();
+        $this->subscription2 = $this->getMockBuilder(Event\Subscription::class)->getMockForAbstractClass();
+        $this->subscription3 = $this->getMockBuilder(Event\Subscription::class)->getMockForAbstractClass();
+        $this->subscription4 = $this->getMockBuilder(Event\Subscription::class)->getMockForAbstractClass();
+
+        $this->output = new ResetSubscriptionsCommandTest\TestOutput();
+    }
+
+    public function testCommand()
+    {
+        $command = new RestartSubscriptionsCommand($this->repository, $this->uow);
+
+        $this->repository
+            ->expects($this->atLeastOnce())
+            ->method('all')
+            ->with()
+            ->willReturn([$this->subscription1, $this->subscription2, $this->subscription3, $this->subscription4])
+        ;
+
+        $this->subscription1
+            ->expects($this->once())
+            ->method('restart')
+        ;
+        $this->subscription1
+            ->expects($this->atLeastOnce())
+            ->method('subscriptionId')
+            ->willReturn(new ResetSubscriptionsCommandTest\SubscriptionId1('ec2be294-c07a-4198-a159-4551686f14f9'))
+        ;
+
+        $this->subscription2
+            ->expects($this->once())
+            ->method('restart')
+            ->willThrowException(new Exception\SubscriptionRestartNotPossible($this->subscription2))
+        ;
+        $this->subscription2
+            ->expects($this->atLeastOnce())
+            ->method('subscriptionId')
+            ->willReturn(new ResetSubscriptionsCommandTest\SubscriptionId2('71150509-d51a-4af1-b541-63c384709452'))
+        ;
+
+        $this->subscription3
+            ->expects($this->once())
+            ->method('restart')
+            ->willThrowException(new \RuntimeException('test exception thrown.'))
+        ;
+        $this->subscription3
+            ->expects($this->atLeastOnce())
+            ->method('subscriptionId')
+            ->willReturn(new ResetSubscriptionsCommandTest\SubscriptionId3('33532423-9d4c-42a8-8abc-7c6ef07face6'))
+        ;
+
+        $this->subscription4
+            ->expects($this->once())
+            ->method('restart')
+        ;
+        $this->subscription4
+            ->expects($this->atLeastOnce())
+            ->method('subscriptionId')
+            ->willReturn(new ResetSubscriptionsCommandTest\SubscriptionId4('4894347a-dfaf-4ee0-83a6-39e650818507'))
+        ;
+
+        $command->run(new StringInput(''), $this->output);
+
+        $expected = <<<OUTPUT
+Subscription Streak\StreakBundle\Tests\Command\ResetSubscriptionsCommandTest\SubscriptionId1(EC2BE294-C07A-4198-A159-4551686F14F9) restart succeeded.
+Subscription Streak\StreakBundle\Tests\Command\ResetSubscriptionsCommandTest\SubscriptionId2(71150509-D51A-4AF1-B541-63C384709452) restart not supported.
+Subscription Streak\StreakBundle\Tests\Command\ResetSubscriptionsCommandTest\SubscriptionId3(33532423-9D4C-42A8-8ABC-7C6EF07FACE6) restart failed with "test exception thrown.".
+Subscription Streak\StreakBundle\Tests\Command\ResetSubscriptionsCommandTest\SubscriptionId4(4894347A-DFAF-4EE0-83A6-39E650818507) restart succeeded.
+
+OUTPUT;
+        $this->assertEquals($expected, $this->output->output);
+    }
+}
+
+namespace Streak\StreakBundle\Tests\Command\ResetSubscriptionsCommandTest;
+
+use Streak\Domain\Id\UUID;
+use Symfony\Component\Console\Output\Output;
+
+class SubscriptionId1 extends UUID
+{
+}
+
+class SubscriptionId2 extends UUID
+{
+}
+
+class SubscriptionId3 extends UUID
+{
+}
+
+class SubscriptionId4 extends UUID
+{
+}
+
+class TestOutput extends Output
+{
+    public $output = '';
+
+    protected function doWrite($message, $newline) : void
+    {
+        $this->output .= $message.($newline ? "\n" : '');
+    }
+}
--- src/Tests/Command/RunSubscriptionsCommandTest.php
+++ src/Tests/Command/RunSubscriptionsCommandTest.php
@@ -16,9 +16,7 @@ namespace Streak\StreakBundle\Tests\Command;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Streak\Domain\Event;
-use Streak\Domain\Event\FilterableStream;
-use Streak\Domain\Event\Listener;
-use Streak\Domain\Event\Sourced\Subscription;
+use Streak\Domain\Event\Stream;
 use Streak\Domain\Event\Subscription\Repository;
 use Streak\Domain\EventStore;
 use Streak\Infrastructure\UnitOfWork;
@@ -36,7 +34,7 @@ class RunSubscriptionsCommandTest extends TestCase
     /**
      * @var Repository|MockObject
      */
-    private $listeners;
+    private $repository;
 
     /**
      * @var EventStore|MockObject
@@ -44,42 +42,27 @@ class RunSubscriptionsCommandTest extends TestCase
     private $store;
 
     /**
-     * @var FilterableStream|MockObject
+     * @var Stream|MockObject
      */
     private $stream;
 
     /**
-     * @var UnitOfWork
+     * @var UnitOfWork|MockObject
      */
     private $uow;
 
     /**
-     * @var Listener|MockObject
-     */
-    private $listener1;
-
-    /**
-     * @var Listener|MockObject
-     */
-    private $listener2;
-
-    /**
-     * @var Listener|MockObject
-     */
-    private $listener3;
-
-    /**
-     * @var Subscription
+     * @var Event\Subscription|MockObject
      */
     private $subscription1;
 
     /**
-     * @var Subscription
+     * @var Event\Subscription|MockObject
      */
     private $subscription2;
 
     /**
-     * @var Subscription
+     * @var Event\Subscription|MockObject
      */
     private $subscription3;
 
@@ -94,136 +77,163 @@ class RunSubscriptionsCommandTest extends TestCase
     private $event2;
 
     /**
+     * @var Event|MockObject
+     */
+    private $event3;
+
+    /**
+     * @var Event|MockObject
+     */
+    private $event4;
+
+    /**
      * @var OutputInterface|MockObject
      */
     private $output;
 
     public function setUp()
     {
-        $this->listeners = $this->getMockBuilder(Repository::class)->getMockForAbstractClass();
+        $this->repository = $this->getMockBuilder(Repository::class)->getMockForAbstractClass();
         $this->store = $this->getMockBuilder(EventStore::class)->getMockForAbstractClass();
-        $this->stream = $this->getMockBuilder(FilterableStream::class)->getMockForAbstractClass();
-        $this->uow = new UnitOfWork\EventStoreUnitOfWork($this->store);
+        $this->stream = $this->getMockBuilder([Stream::class, \IteratorAggregate::class])->getMock();
+        $this->uow = $this->getMockBuilder(UnitOfWork::class)->getMockForAbstractClass();
 
-        $this->listener1 = $this->getMockBuilder(Listener::class)->getMockForAbstractClass();
-        $this->listener2 = $this->getMockBuilder(Listener::class)->getMockForAbstractClass();
-        $this->listener3 = $this->getMockBuilder(Listener::class)->getMockForAbstractClass();
-
-        $this->subscription1 = new Subscription($this->listener1);
-        $this->subscription2 = new Subscription($this->listener2);
-        $this->subscription3 = new Subscription($this->listener3);
+        $this->subscription1 = $this->getMockBuilder([Event\Subscription::class, Event\Producer::class])->getMock();
+        $this->subscription2 = $this->getMockBuilder([Event\Subscription::class, Event\Producer::class])->getMock();
+        $this->subscription3 = $this->getMockBuilder([Event\Subscription::class, Event\Producer::class])->getMock();
 
         $this->event1 = $this->getMockBuilder(Event::class)->setMockClassName('event1')->getMockForAbstractClass();
         $this->event2 = $this->getMockBuilder(Event::class)->setMockClassName('event2')->getMockForAbstractClass();
+        $this->event3 = $this->getMockBuilder(Event::class)->setMockClassName('event3')->getMockForAbstractClass();
+        $this->event4 = $this->getMockBuilder(Event::class)->setMockClassName('event4')->getMockForAbstractClass();
 
-        $this->subscription1->startFor($this->event1, new \DateTime());
-        $this->subscription2->startFor($this->event1, new \DateTime());
-        $this->subscription3->startFor($this->event1, new \DateTime());
-
-        $this->output = $this->getMockBuilder(OutputInterface::class)->getMockForAbstractClass();
+        $this->output = new RunSubscriptionsCommandTest\TestOutput();
     }
 
     public function testCommand()
     {
-        $command = new RunSubscriptionsCommand($this->listeners, $this->store, $this->uow);
+        $command = new RunSubscriptionsCommand($this->repository, $this->store, $this->uow);
 
-        $this->listeners
+        $this->repository
             ->expects($this->atLeastOnce())
             ->method('all')
             ->with()
             ->willReturn([$this->subscription1, $this->subscription2, $this->subscription3])
         ;
 
-        $this->store
+        $this->subscription1
+            ->expects($this->once())
+            ->method('subscribeTo')
+            ->with($this->store)
+            ->willReturn([$this->event1, $this->event2])
+        ;
+        $this->subscription1
+            ->expects($this->atLeastOnce())
+            ->method('subscriptionId')
+            ->willReturn(new RunSubscriptionsCommandTest\SubscriptionId1('ec2be294-c07a-4198-a159-4551686f14f9'))
+        ;
+
+        $this->subscription2
+            ->expects($this->once())
+            ->method('subscribeTo')
+            ->with($this->store)
+            ->willReturnCallback(function () {
+                yield $this->event1;
+                throw new \RuntimeException('test exception thrown.');
+            })
+        ;
+        $this->subscription2
+            ->expects($this->atLeastOnce())
+            ->method('subscriptionId')
+            ->willReturn(new RunSubscriptionsCommandTest\SubscriptionId2('71150509-d51a-4af1-b541-63c384709452'))
+        ;
+
+        $this->subscription3
+            ->expects($this->once())
+            ->method('subscribeTo')
+            ->with($this->store)
+            ->willReturn([$this->event1, $this->event2, $this->event3, $this->event4])
+        ;
+        $this->subscription3
+            ->expects($this->atLeastOnce())
+            ->method('subscriptionId')
+            ->willReturn(new RunSubscriptionsCommandTest\SubscriptionId3('33532423-9d4c-42a8-8abc-7c6ef07face6'))
+        ;
+
+        $this->uow
+            ->expects($this->exactly(9))
+            ->method('commit')
+        ;
+        $this->uow
             ->expects($this->exactly(3))
-            ->method('stream')
-            ->with()
-            ->willReturnOnConsecutiveCalls(
-                $this->stream,
-                $this->stream,
-                $this->stream
-            )
+            ->method('clear')
         ;
 
         $this->isIteratorFor($this->stream, [$this->event2]);
 
-        $this->listener1
-            ->expects($this->once())
-            ->method('on')
-            ->with($this->event2)
-        ;
-
-        $exception = new \RuntimeException('Test exception.');
-
-        $this->listener2
-            ->expects($this->once())
-            ->method('on')
-            ->with($this->event2)
-            ->willThrowException($exception)
-        ;
-
-        $this->listener3
-            ->expects($this->once())
-            ->method('on')
-            ->with($this->event2)
-        ;
-
-        $this->output
-            ->expects($this->exactly(2))
-            ->method('writeln')
-            ->withConsecutive(
-                ['Test exception.'],
-                [$this->isType('string')]
-            )
-        ;
-
         $command->run(new StringInput(''), $this->output);
+
+        $expected = <<<OUTPUT
+Running Streak\StreakBundle\Tests\Command\RunSubscriptionsCommandTest\SubscriptionId1(EC2BE294-C07A-4198-A159-4551686F14F9) subscription:
+
+    1 [->--------------------------]
+    2 [-->-------------------------]
+
+Running Streak\StreakBundle\Tests\Command\RunSubscriptionsCommandTest\SubscriptionId2(71150509-D51A-4AF1-B541-63C384709452) subscription:
+
+    1 [->--------------------------]
+Subscription Streak\StreakBundle\Tests\Command\RunSubscriptionsCommandTest\SubscriptionId2(71150509-D51A-4AF1-B541-63C384709452) failed with "test exception thrown.".
+
+
+Running Streak\StreakBundle\Tests\Command\RunSubscriptionsCommandTest\SubscriptionId3(33532423-9D4C-42A8-8ABC-7C6EF07FACE6) subscription:
+
+    1 [->--------------------------]
+    2 [-->-------------------------]
+    3 [--->------------------------]
+    4 [---->-----------------------]
+
+
+OUTPUT;
+        $this->assertEquals($expected, $this->output->output);
     }
 
-    private function isIteratorFor(MockObject $iterator, array $items)
+    private function isIteratorFor(MockObject $mock, array $items)
     {
-        $internal = new \ArrayIterator($items);
+        $iterator = new \ArrayIterator($items);
 
-        $iterator
+        $mock
             ->expects($this->any())
-            ->method('rewind')
-            ->willReturnCallback(function () use ($internal) {
-                $internal->rewind();
-            })
+            ->method('getIterator')
+            ->willReturn($iterator)
         ;
 
-        $iterator
-            ->expects($this->any())
-            ->method('current')
-            ->willReturnCallback(function () use ($internal) {
-                return $internal->current();
-            })
-        ;
+        return $mock;
+    }
+}
 
-        $iterator
-            ->expects($this->any())
-            ->method('key')
-            ->willReturnCallback(function () use ($internal) {
-                return $internal->key();
-            })
-        ;
+namespace Streak\StreakBundle\Tests\Command\RunSubscriptionsCommandTest;
 
-        $iterator
-            ->expects($this->any())
-            ->method('next')
-            ->willReturnCallback(function () use ($internal) {
-                $internal->next();
-            })
-        ;
+use Streak\Domain\Id\UUID;
+use Symfony\Component\Console\Output\Output;
 
-        $iterator
-            ->expects($this->any())
-            ->method('valid')
-            ->willReturnCallback(function () use ($internal) {
-                return $internal->valid();
-            })
-        ;
+class SubscriptionId1 extends UUID
+{
+}
 
-        return $iterator;
+class SubscriptionId2 extends UUID
+{
+}
+
+class SubscriptionId3 extends UUID
+{
+}
+
+class TestOutput extends Output
+{
+    public $output = '';
+
+    protected function doWrite($message, $newline) : void
+    {
+        $this->output .= $message.($newline ? "\n" : '');
     }
 }
