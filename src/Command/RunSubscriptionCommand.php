add5b41bff6ca7a5fa3a115cdae7c7b633fb32bc
Alan Gabriel Bem <alan.bem@gmail.com>
2018-10-07 18:02:18 +0200

    full list of changes below: - added 'streak:subscriptions:run' command that runs single subscription - enhanced 'streak:subscriptions:run' command with ability to run each subscription asynchronously - enhanced 'streak:subscriptions:restart' command with ability to include completed subscriptions

---
+++ src/Command/RunSubscriptionCommand.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * This file is part of the streak-bundle package.
+ *
+ * (C) Alan Gabriel Bem <alan.bem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Streak\StreakBundle\Command;
+
+use Streak\Domain\Event\Subscription\Repository;
+use Streak\Domain\EventStore;
+use Streak\Domain\Id;
+use Streak\Infrastructure\UnitOfWork;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutputInterface;
+use Symfony\Component\Console\Output\ConsoleSectionOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\StreamOutput;
+
+/**
+ * @author Alan Gabriel Bem <alan.bem@gmail.com>
+ */
+class RunSubscriptionCommand extends Command
+{
+    private $subscriptions;
+    private $store;
+    private $uow;
+
+    public function __construct(Repository $subscriptions, EventStore $store, UnitOfWork $uow)
+    {
+        $this->subscriptions = $subscriptions;
+        $this->store = $store;
+        $this->uow = $uow;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('streak:subscription:run');
+        $this->setDescription('Runs single subscription (sagas, process managers, projectors, etc)');
+        $this->setDefinition([
+            new InputArgument('subscription-type', InputArgument::REQUIRED, 'Specify subscription type'),
+            new InputArgument('subscription-id', InputArgument::REQUIRED, 'Specify subscription id'),
+        ]);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $subscription = $this->subscriptions->find($this->id($input));
+
+        if (null === $subscription) {
+            $output->writeln(sprintf('Subscription <fg=blue>%s</>(<fg=cyan>%s</>) not found.', $input->getArgument('subscription-type'), $input->getArgument('subscription-id')));
+
+            return;
+        }
+
+        ProgressBar::setFormatDefinition('custom', 'Subscription <fg=blue>%subscription_type%</>(<fg=cyan>%subscription_id%</>) processed <fg=yellow>%current%</> events in <fg=magenta>%elapsed%</>.');
+
+        // progress bar by default is using stderr, lets avoid that
+        if ($output instanceof ConsoleOutputInterface) {
+            $output = $output->section(); // @codeCoverageIgnore
+        } elseif ($output instanceof StreamOutput) {
+            $output = new StreamOutput($output->getStream(), $output->getVerbosity(), null, $output->getFormatter()); // @codeCoverageIgnore
+        }
+
+        $progress = new ProgressBar($output);
+        $progress->setFormat('custom');
+        $progress->setOverwrite(true);
+        $progress->setMessage($input->getArgument('subscription-type'), 'subscription_type');
+        $progress->setMessage($input->getArgument('subscription-id'), 'subscription_id');
+        $progress->display();
+        $output->writeln(''); // streak:subscriptions:run requires this for splitting the output
+
+        try {
+            foreach ($subscription->subscribeTo($this->store) as $event) {
+                iterator_to_array($this->uow->commit());
+                $this->uow->add($subscription);
+                if ($output instanceof ConsoleSectionOutput) {
+                    $output->clear(); // @codeCoverageIgnore
+                }
+                $progress->advance();
+                $output->writeln(''); // streak:subscriptions:run requires this for splitting the output
+            }
+            iterator_to_array($this->uow->commit());
+        } finally {
+            $this->uow->clear();
+            if ($output instanceof ConsoleSectionOutput) {
+                $output->clear(); // @codeCoverageIgnore
+            }
+            $progress->finish();
+        }
+    }
+
+    private function id(InputInterface $input) : Id
+    {
+        $class = $input->getArgument('subscription-type');
+        $value = $input->getArgument('subscription-id');
+
+        return call_user_func([$class, 'fromString'], $value);
+    }
+}
